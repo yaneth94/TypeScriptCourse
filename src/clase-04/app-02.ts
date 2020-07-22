cb035c870020e69491da45300a5668ab46677534
Zoila Villatoro <zoilavillatoro6694@gmail.com>
2020-07-21 19:39:42 -0600

    added POO in typescript

---
+++ src/clase-04/app-02.ts
@@ -0,0 +1,39 @@
+export {}
+// MIEMBROS PUBLICOS
+
+class Curso {
+  public id: number // public
+  public nombre: string // public
+
+  constructor (id: number, nombre: string) {
+    this.id = id
+    this.nombre = nombre
+  }
+}
+
+class EscuelaDigital {
+  // Atributos
+  public nombre: string //public
+  public cursos: Curso[] = [] //public
+
+  // Funcion que nos permite crear objetos
+  constructor (nombre: string) {
+    this.nombre = nombre
+  }
+
+  public agregarCurso (curso: Curso) {
+    this.cursos.push(curso)
+  }
+}
+
+const escuela = new EscuelaDigital('EDteam')
+let cursoJS = new Curso(1, 'JavaScript')
+let cursoTS = new Curso(2, 'TypeScript')
+escuela.agregarCurso(cursoJS)
+escuela.agregarCurso(cursoTS)
+// Acceder a los atributos publicos
+// modificando los valores desde afuera porque los atributos son publicos
+cursoJS.nombre = 'JavaScript desde Cero'
+cursoJS.id = 20
+console.log('escuela', escuela)
+console.log('escuela.nombre', escuela.nombre)
